e7f177d704b818a5179cc97c6015cad45642590d
Dmitry <kunutok@mail.ru>
2022-11-16 00:03:53 +0300

    Change today component
    Remove img today
    Added padding bottom
    added new component bullet

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,14 +1,34 @@
-import today from "../../public/today.svg";
+import { Bullet } from "@nivo/bullet";
 
 const Today = () => {
   const timeIsToday = JSON.parse(localStorage.getItem("DataBase") || "{}");
   return (
-    <div className="px-4 pt-4 rounded-2xl bg-grayish-500">
+    <div className="px-4 py-4 rounded-2xl bg-grayish-500">
       <a href="">
         <div className="font-bold">Today</div>
         Time is {timeIsToday.time} minuts
-        <div>
-          <img src={today} />
+        <div className="min-w-0">
+          <Bullet
+            data={[
+              {
+                id: "",
+                ranges: [0, 10, 20, 60],
+                measures: [timeIsToday.time],
+                markers: [20],
+              },
+            ]}
+            minValue="auto"
+            maxValue="auto"
+            rangeBorderWidth={10}
+            measureBorderColor={{ from: "color", modifiers: [] }}
+            measureBorderWidth={10}
+            markerSize={1}
+            rangeColors="blues"
+            measureColors="seq:blues"
+            markerColors="seq:yellow_orange_brown"
+            height={50}
+            width={300}
+          />
         </div>
       </a>
     </div>
