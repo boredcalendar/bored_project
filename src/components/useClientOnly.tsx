b186f3b3a7456228f9e56c45d813e0a542ba401f
Yaroslav Lapin <ylapin@legalzoom.com>
2026-05-30 09:57:05 -0600

    Remove duplicate prototype components (#14)

--- src/components/useClientOnly.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function useClientOnly() {
-  const [hasMounted, setHasMounted] = React.useState(false);
-  React.useEffect(() => {
-    setHasMounted(true);
-  }, []);
-  return hasMounted;
-}
